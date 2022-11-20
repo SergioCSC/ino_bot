8e7dcbeb8999d573c311d3d7fe35a2575cbd8e3a
Sergei Epifanov <pifych@gmail.com>
2022-11-20 18:34:56 +0400

    simple echo bot

---
+++ hello_bot.py
@@ -0,0 +1,17 @@
+import json
+from telegram import Update
+from telegram.ext import Updater, CallbackContext, TypeHandler
+
+def echo(update: Update, context: CallbackContext) -> None:
+    text = json.dumps(update.to_dict(), indent=2)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+def main() -> None:
+    updater = Updater('5781055986:AAGuGUbgTk0MtZlydHFPltp2DEBHFX_QDTM')
+    updater.dispatcher.add_handler(TypeHandler(Update, echo))
+    updater.start_polling()
+    print('Started')
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
