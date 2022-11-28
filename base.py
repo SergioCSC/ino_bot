1c2d7c3691a4bd3ad1867bc41116cff5dbe2661c
Sergei Epifanov <pifych@gmail.com>
2022-11-29 01:43:28 +0400

    add /del and /list commands

--- base.py
+++ base.py
@@ -1,7 +1,13 @@
+from enum import Enum, auto
 import config as cfg
 
 import json
 import pathlib
+
+
+class Command(Enum):
+    UPDATE = auto()
+    DELETE = auto()
 
 
 def retrieve_inos(chat_id: int) -> dict[str, int]:
@@ -16,14 +22,23 @@ def _retrieve_all_chats_inos() -> dict[int, dict[str, int]]:
     with json_path.open() as f:  #TODO what if no file
         dict_with_str_keys = json.load(f)
         return {int(chat_id): v for chat_id, v in dict_with_str_keys.items()}
-        
+
+
+def _manipulate_inos(chat_id: int, new_inos: dict[str, int], command: Command) -> None:
     
+    def _manipulate(_old_inos: dict[str, int], _new_inos: dict[str, int], command: Command) \
+            -> dict[str, int]:
 
-def update_inos(chat_id: int, new_inos: dict[str, int]) -> None:
-
-    def _rewrite_all_chats_inos(inos: dict[int, dict[str, int]]) -> None:
-        with open(cfg.INOS_JSON_FILENAME, 'w') as f:
-            json.dump(inos, f)
+        match command:
+            case Command.UPDATE:
+                return _old_inos | _new_inos
+            case Command.DELETE:
+                for ino in _new_inos:
+                    if ino in _old_inos:
+                        del _old_inos[ino]
+                return _old_inos
+            case _:
+                raise NotImplementedError('Unknown database command')
 
     if not new_inos:
         return
@@ -31,16 +46,51 @@ def update_inos(chat_id: int, new_inos: dict[str, int]) -> None:
     new_inos = _clear(new_inos)
 
     all_chats_inos = _retrieve_all_chats_inos()
+    old_inos = all_chats_inos.get(chat_id, {}).copy()
 
-    old_inos = all_chats_inos.get(chat_id, {})
-    new_inos = old_inos | new_inos
+    new_inos = _manipulate(old_inos, new_inos, command)
+    
     all_chats_inos[chat_id] = new_inos
-
     _rewrite_all_chats_inos(all_chats_inos)
 
 
-def _clear(inos: dict[str, int]) -> dict[str, int]:
+def update_inos(chat_id: int, new_inos: dict[str, int]) -> None:
+
+    _manipulate_inos(chat_id, new_inos, Command.UPDATE)
+    # if not new_inos:
+    #     return
     
+    # new_inos = _clear(new_inos)
+
+    # all_chats_inos = _retrieve_all_chats_inos()
+
+    # old_inos = all_chats_inos.get(chat_id, {})
+    # new_inos = old_inos | new_inos
+    # all_chats_inos[chat_id] = new_inos
+
+    # _rewrite_all_chats_inos(all_chats_inos)
+
+
+def delete_inos(chat_id: int, del_inos: dict[str, int]) -> None:
+
+    _manipulate_inos(chat_id, del_inos, Command.DELETE)
+    # if not del_inos:
+    #     return
+
+    # del_inos = _clear(del_inos)
+    
+    # all_chats_inos = _retrieve_all_chats_inos()
+    # old_inos = all_chats_inos.get(chat_id, {})
+    
+    # for ino in del_inos:
+    #     if ino in old_inos:
+    #         del old_inos[ino]
+
+    # _rewrite_all_chats_inos(all_chats_inos)
+
+
+def _clear(inos: dict[str, int]) -> dict[str, int]:
+
     def _clear_name(name: str) -> str:
         for prefix in cfg.INO_PREFIXES:
             if name.startswith(prefix):
@@ -48,3 +98,8 @@ def _clear(inos: dict[str, int]) -> dict[str, int]:
         return name
 
     return {_clear_name(name): post_id for name, post_id in inos.items()}
+
+
+def _rewrite_all_chats_inos(inos: dict[int, dict[str, int]]) -> None:
+    with open(cfg.INOS_JSON_FILENAME, 'w') as f:
+        json.dump(inos, f)
--- tg_api_connection.py
+++ tg_api_connection.py
@@ -25,16 +25,36 @@ def send_ino_updates(update: Update, context: CallbackContext) -> None:
     base.update_inos(chat_id, new_inos)
 
 
-def add_ino_command(update: Update, context: CallbackContext) -> None:
-    inos = update.message.text.split()[1:]
+def _get_inos_from_update(update: Update) -> dict[str, int]:
+    inos = update.effective_message.text.split()[1:]
     inos = {name: -1 for name in inos}
+    return inos
+
+
+def add_ino_command(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
+    inos = _get_inos_from_update(update)
     base.update_inos(chat_id, inos)
+
+def del_ino_command(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    inos = _get_inos_from_update(update)
+    base.delete_inos(chat_id, inos)
+
+def list_ino_command(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    inos = base.retrieve_inos(chat_id)
+    text = ' '.join(ino_name for ino_name in inos)
+    context.bot.send_message(
+                chat_id=chat_id, text=text
+            )
 
 
 def main() -> None:
     updater = Updater('5781055986:AAGuGUbgTk0MtZlydHFPltp2DEBHFX_QDTM')
     updater.dispatcher.add_handler(CommandHandler('add', add_ino_command))
+    updater.dispatcher.add_handler(CommandHandler('del', del_ino_command))
+    updater.dispatcher.add_handler(CommandHandler('list', list_ino_command))
     updater.dispatcher.add_handler(TypeHandler(Update, send_ino_updates))
     updater.start_polling()
     print('Started')
