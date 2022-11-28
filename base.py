443a79838ba5545b54dfc6591a06aa5b47315943
Sergei Epifanov <pifych@gmail.com>
2022-11-28 17:50:57 +0400

    store post_id's for each chat_id. txt --> json

--- base.py
+++ base.py
@@ -1,28 +1,37 @@
 import config as cfg
 
-
-def retrieve_inos() -> dict[str, int]:
-    with open(cfg.INOS_FILENAME) as f:
-        result = (ino.strip().split() for ino in f if ino.strip())
-        result = (ino if len(ino) > 1 else (ino[0], -1) for ino in result)
-        result = {name: int(post_id) for name, post_id in result}
-        return result
+import json
+import pathlib
 
 
-def update_inos(new_inos: dict[str, int]) -> None:
+def retrieve_inos(chat_id: int) -> dict[str, int]:
+    all_chats_inos = _retrieve_all_chats_inos()
+    return all_chats_inos.get(chat_id, {})
+
+
+def _retrieve_all_chats_inos() -> dict[int, dict[str, int]]:
+    json_path = pathlib.Path(cfg.INOS_JSON_FILENAME)
+    if not json_path.is_file():
+        return {}
+    with json_path.open() as f:  #TODO what if no file
+        dict_with_str_keys = json.load(f)
+        return {int(chat_id): v for chat_id, v in dict_with_str_keys.items()}
+        
     
-    def rewrite_inos(inos: dict[str, int]) -> None:
-        with open(cfg.INOS_FILENAME, 'w') as f:
-            for ino_name, ino_post_id in inos.items():
-                f.write(f'{ino_name} {ino_post_id}\n')
 
-    old_inos = retrieve_inos()
-    all_inos = old_inos | new_inos
-    rewrite_inos(all_inos)
+def update_inos(chat_id: int, new_inos: dict[str, int]) -> None:
 
+    def _rewrite_all_chats_inos(inos: dict[int, dict[str, int]]) -> None:
+        with open(cfg.INOS_JSON_FILENAME, 'w') as f:
+            json.dump(inos, f)
 
-def _clear_inos(inos: dict[str, int]) -> dict[str, int]:
-    cleared_inos = {name.strip(): post_id for name, post_id in inos.items()}
-    if '' in cleared_inos:
-        del cleared_inos['']
-    return cleared_inos
+    if not new_inos:
+        return
+
+    all_chats_inos = _retrieve_all_chats_inos()
+
+    old_inos = all_chats_inos.get(chat_id, {})
+    new_inos = old_inos | new_inos
+    all_chats_inos[chat_id] = new_inos
+
+    _rewrite_all_chats_inos(all_chats_inos)
--- tg_api_connection.py
+++ tg_api_connection.py
@@ -7,24 +7,27 @@ from telegram.ext import Updater, CallbackContext, TypeHandler, CommandHandler
 
 
 def send_ino_updates(update: Update, context: CallbackContext) -> None:
-    inos = base.retrieve_inos()
+    chat_id = update.effective_chat.id
+    inos = base.retrieve_inos(chat_id)
     new_inos = {}
     for ino in inos.items():
-        texts, last_post_id = http_get_ino.get_last_posts_and_last_post_id(ino)
-        ino_name, _ = ino
-        new_inos[ino_name] = last_post_id
+        texts, new_last_post_id = http_get_ino.get_last_posts_and_last_post_id(ino)
+        ino_name, old_last_post_id = ino
+        if new_last_post_id > old_last_post_id:
+            new_inos[ino_name] = new_last_post_id
         for text in texts:
             context.bot.send_message(
-                chat_id=update.effective_chat.id, text=text,
+                chat_id=chat_id, text=text,
                 parse_mode=ParseMode.HTML
             )
-    base.update_inos(new_inos)
+    base.update_inos(chat_id, new_inos)
 
 
 def add_ino_command(update: Update, context: CallbackContext) -> None:
     inos = update.message.text.split()[1:]
     inos = {name: -1 for name in inos}
-    base.update_inos(inos)
+    chat_id = update.effective_chat.id
+    base.update_inos(chat_id, inos)
 
 
 def main() -> None:
