55f13b981e2afa15ed71c1c304cf9261143c2af9
Sergei Epifanov <pifych@gmail.com>
2022-12-02 05:53:22 +0400

    /clear and /clearall commands

--- tg_api_connector.py
+++ tg_api_connector.py
@@ -10,7 +10,7 @@ from telegram.ext import Updater
 from telegram.ext import TypeHandler, CommandHandler
 
 
-def send_ino_updates(update: Update, context: CallbackContext) -> None:
+def send_inos_updates(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     inos = base.retrieve_inos(chat_id)
     new_inos = {}
@@ -28,29 +28,49 @@ def send_ino_updates(update: Update, context: CallbackContext) -> None:
     base.update_inos(chat_id, new_inos)
 
 
-def _get_inos_from_update(update: Update) -> dict[str, int]:
-    inos = update.effective_message.text.split()[1:]
-    inos = {name: -1 for name in inos}
-    return inos
-
-
-def add_ino_command(update: Update, context: CallbackContext) -> None:
+def add_inos_command(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     inos = _get_inos_from_update(update)
     base.update_inos(chat_id, inos)
 
 
-def del_ino_command(update: Update, context: CallbackContext) -> None:
+def del_inos_command(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     inos = _get_inos_from_update(update)
     base.delete_inos(chat_id, inos)
 
 
-def list_ino_command(update: Update, context: CallbackContext) -> None:
+def list_inos_command(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
     inos = base.retrieve_inos(chat_id)
     text = ' '.join(ino_name for ino_name in inos)
     context.bot.send_message(chat_id=chat_id, text=text)
+
+
+def clear_inos_command(update: Update, context: CallbackContext) -> None:
+    # del_ino_command(update, CallbackContext)
+    # add_ino_command(update, CallbackContext)
+    chat_id = update.effective_chat.id
+    inos = _get_inos_from_update(update)
+    _clear_inos(chat_id, inos)
+
+
+def clear_all_inos_command(update: Update, context: CallbackContext) -> None:
+    chat_id = update.effective_chat.id
+    inos = base.retrieve_inos(chat_id)
+    _clear_inos(chat_id, inos)
+
+
+def _clear_inos(chat_id: int, inos: dict[str, int]) -> None:
+    base.delete_inos(chat_id, inos)
+    inos = {name: -1 for name in inos}
+    base.update_inos(chat_id, inos)
+
+
+def _get_inos_from_update(update: Update) -> dict[str, int]:
+    inos = update.effective_message.text.split()[1:]
+    inos = {name: -1 for name in inos}
+    return inos
 
 
 def read_token() -> str:
@@ -60,10 +80,12 @@ def read_token() -> str:
 
 def start_long_polling():
     updater = Updater(read_token())
-    updater.dispatcher.add_handler(CommandHandler('add', add_ino_command))
-    updater.dispatcher.add_handler(CommandHandler('del', del_ino_command))
-    updater.dispatcher.add_handler(CommandHandler('list', list_ino_command))
-    updater.dispatcher.add_handler(TypeHandler(Update, send_ino_updates))
+    updater.dispatcher.add_handler(CommandHandler('add', add_inos_command))
+    updater.dispatcher.add_handler(CommandHandler('del', del_inos_command))
+    updater.dispatcher.add_handler(CommandHandler('clear', clear_inos_command))
+    updater.dispatcher.add_handler(CommandHandler('clearall', clear_all_inos_command))
+    updater.dispatcher.add_handler(CommandHandler('list', list_inos_command))
+    updater.dispatcher.add_handler(TypeHandler(Update, send_inos_updates))
     updater.start_polling()
     print('Started')
     updater.idle()
