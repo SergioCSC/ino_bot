c766c92c7d82144c9fba3d08463db03ab59c84fb
Sergei Epifanov <pifych@gmail.com>
2022-12-07 07:53:38 +0400

    move get_new_messages() logic from tg_api_connector.py:get_inos_updates() to model

--- main.py
+++
@@ -1,9 +0,0 @@
-import tg_api_connector
-
-
-def main() -> None:
-    tg_api_connector.start_long_polling()
-
-
-if __name__ == '__main__':
-    main()
--- tg_api_connector.py
+++ tg_api_connector.py
@@ -1,6 +1,6 @@
 import config as cfg
-import http_get_posts
-import base
+import model
+import base  # TODO refuse to use the database directly in this module
 
 import time
 from telegram import Update
@@ -10,22 +10,15 @@ from telegram.ext import Updater
 from telegram.ext import TypeHandler, CommandHandler
 
 
-def send_inos_updates(update: Update, context: CallbackContext) -> None:
+def get_inos_updates(update: Update, context: CallbackContext) -> None:
     chat_id = update.effective_chat.id
-    inos = base.retrieve_inos(chat_id)
-    new_inos = {}
-    for ino in inos.items():
-        texts, new_last_post_id = http_get_posts.get_last_posts_and_last_post_id(ino)
-        ino_name, old_last_post_id = ino
-        if new_last_post_id > old_last_post_id:
-            new_inos[ino_name] = new_last_post_id
-        for text in texts:
-            context.bot.send_message(
-                chat_id=chat_id, text=text,
-                parse_mode=ParseMode.HTML
-            )
-            time.sleep(1)
-    base.update_inos(chat_id, new_inos)
+    texts = model.get_new_messages(chat_id)
+    for text in texts:
+        context.bot.send_message(
+            chat_id=chat_id, text=text,
+            parse_mode=ParseMode.HTML
+        )
+        time.sleep(1)
 
 
 def add_inos_command(update: Update, context: CallbackContext) -> None:
@@ -85,7 +78,11 @@ def start_long_polling():
     updater.dispatcher.add_handler(CommandHandler('clear', clear_inos_command))
     updater.dispatcher.add_handler(CommandHandler('clearall', clear_all_inos_command))
     updater.dispatcher.add_handler(CommandHandler('list', list_inos_command))
-    updater.dispatcher.add_handler(TypeHandler(Update, send_inos_updates))
+    updater.dispatcher.add_handler(TypeHandler(Update, get_inos_updates))
     updater.start_polling()
     print('Started')
     updater.idle()
+
+
+if __name__ == '__main__':
+    start_long_polling()
