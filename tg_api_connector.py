5254df1715bf245e8182058a6ede0006c1ce1233
Sergei Epifanov <pifych@gmail.com>
2022-12-16 18:38:51 +0400

    move to python-telegram-bot v20, aws compatible

--- tg_api_connector.py
+++ tg_api_connector.py
@@ -2,54 +2,65 @@ import config as cfg
 import model
 import base  # TODO refuse to use the database directly in this module
 
+import asyncio
+import json
 import time
+
+import pathlib
+import sys
+lib_folder = pathlib.Path(__file__).parent / 'libs_for_aws_lambda'
+sys.path.append(str(lib_folder))
+
 from telegram import Update
-from telegram import ParseMode
-from telegram.ext import CallbackContext
+from telegram.constants import ParseMode
+from telegram.ext import ContextTypes
 from telegram.ext import Updater
 from telegram.ext import TypeHandler, CommandHandler
 
+from telegram.ext import Application
+from telegram.ext import ApplicationBuilder
 
-def get_inos_updates(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
+
+async def get_inos_updates(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id  # type: ignore
     texts = model.get_new_messages(chat_id)
     for text in texts:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=chat_id, text=text,
             parse_mode=ParseMode.HTML
         )
         time.sleep(1)
 
 
-def add_inos_command(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
+async def add_inos_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id  # type: ignore
     inos = _get_inos_from_update(update)
     base.update_inos(chat_id, inos)
 
 
-def del_inos_command(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
+async def del_inos_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id  # type: ignore
     inos = _get_inos_from_update(update)
     base.delete_inos(chat_id, inos)
 
 
-def list_inos_command(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
+async def list_inos_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id  # type: ignore
     inos = base.retrieve_inos(chat_id)
     text = ' '.join(ino_name for ino_name in inos)
-    context.bot.send_message(chat_id=chat_id, text=text)
+    await context.bot.send_message(chat_id=chat_id, text=text)
 
 
-def clear_inos_command(update: Update, context: CallbackContext) -> None:
+async def clear_inos_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # del_ino_command(update, CallbackContext)
     # add_ino_command(update, CallbackContext)
-    chat_id = update.effective_chat.id
+    chat_id = update.effective_chat.id  # type: ignore
     inos = _get_inos_from_update(update)
     _clear_inos(chat_id, inos)
 
 
-def clear_all_inos_command(update: Update, context: CallbackContext) -> None:
-    chat_id = update.effective_chat.id
+async def clear_all_inos_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_chat.id  # type: ignore
     inos = base.retrieve_inos(chat_id)
     _clear_inos(chat_id, inos)
 
@@ -61,7 +72,7 @@ def _clear_inos(chat_id: int, inos: dict[str, int]) -> None:
 
 
 def _get_inos_from_update(update: Update) -> dict[str, int]:
-    inos = update.effective_message.text.split()[1:]
+    inos = update.effective_message.text.split()[1:]  # type: ignore
     inos = {name: -1 for name in inos}
     return inos
 
@@ -71,18 +82,46 @@ def read_token() -> str:
         return f.read().strip()
 
 
-def start_long_polling():
-    updater = Updater(read_token())
-    updater.dispatcher.add_handler(CommandHandler('add', add_inos_command))
-    updater.dispatcher.add_handler(CommandHandler('del', del_inos_command))
-    updater.dispatcher.add_handler(CommandHandler('clear', clear_inos_command))
-    updater.dispatcher.add_handler(CommandHandler('clearall', clear_all_inos_command))
-    updater.dispatcher.add_handler(CommandHandler('list', list_inos_command))
-    updater.dispatcher.add_handler(TypeHandler(Update, get_inos_updates))
-    updater.start_polling()
-    print('Started')
-    updater.idle()
+def get_application_with_handlers() -> Application:
+    # application = ApplicationBuilder().token(read_token()).build()
+    application = Application.builder().token(read_token()).build()
+    application.add_handler(CommandHandler('add', add_inos_command))
+    application.add_handler(CommandHandler('del', del_inos_command))
+    application.add_handler(CommandHandler('clear', clear_inos_command))
+    application.add_handler(CommandHandler('clearall', clear_all_inos_command))
+    application.add_handler(CommandHandler('list', list_inos_command))
+    application.add_handler(TypeHandler(Update, get_inos_updates))
+    return application
+
+
+def handle_updates_via_webhook(event, context):
+    # application = get_application_with_handlers()
+    # update = Update.de_json(json.loads(event['body']), application.bot)
+    # await application.process_update(update)
+    # return asyncio.get_event_loop().run_until_complete(main(event, context))
+    return asyncio.run(main(event, context))
+
+
+async def main(event, context):
+    application = get_application_with_handlers()
+
+    try:
+        await application.initialize()
+        update = Update.de_json(json.loads(event['body']), application.bot)
+        await application.process_update(update)
+
+        return {
+            'statusCode': 200,
+            'body': 'Success'
+        }
+
+    except Exception:
+        return {
+            'statusCode': 500,
+            'body': 'Failure'
+        }
 
 
 if __name__ == '__main__':
-    start_long_polling()
+    application = get_application_with_handlers()
+    application.run_polling()
